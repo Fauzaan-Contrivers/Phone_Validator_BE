c6b4c317ee266ef4f86b5c93fb7cc83b11a77efa
Awais khan <awais@contrivers.dev>
2024-03-08 19:14:16 +0500

    Database Timeout Fixed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors();
+  const server = app.getHttpServer();
+  const expressApp = server.getHttpServer();
+  expressApp.keepAliveTimeout = 600000;
   await app.listen(8000);
 }
 bootstrap();
