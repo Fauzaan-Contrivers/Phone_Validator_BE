ac48a11e8bd79b33be31e6a1e850c594961022fa
Awais khan <awais@contrivers.dev>
2024-03-08 18:57:57 +0500

    Database Timeout Fixed

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -13,7 +13,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   username: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  connectTimeout: 6000000,
+  connectTimeout: 28800,
   entities: [Users, Sheets, UserSheets],
   logging: false,
   synchronize: true,
