2f2a2a1ef778afa7bd72e1dc8b8c50bfe0545a34
Fauzaan <fauzaan@hemavi.com>
2024-03-08 18:40:24 +0500

    Basic Performance improvements

--- src/sheets/sheets.controller.ts
+++ src/sheets/sheets.controller.ts
@@ -1,41 +1,47 @@
-import { Controller, Get, UseGuards, Req, Res, Logger, Put, Body, Param, UseInterceptors, UploadedFile, Post } from '@nestjs/common';
-import { SheetsService } from './sheets.service';
+import { Controller, Post, Param, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Multer,diskStorage } from 'multer';
-import { extname } from 'path'
-
+import { Multer } from 'multer';
+import { SheetsService } from './sheets.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('sheets')
 export class SheetsController {
-    constructor(
-        private readonly sheetsService: SheetsService) {}
+  constructor(private readonly sheetsService: SheetsService) { }
 
-        @Post('upload/:id')
-        @UseInterceptors(FileInterceptor('image')) 
-        async updateSheet(
-          @Param('id') id: number,
-          @UploadedFile() image: Multer.File, 
-        ) {
-          // Pass image along with other payload data to service for update
-          return this.sheetsService.updateSheet(id, image);
-        }
-        @Post('upload-file/:id')
-        @UseInterceptors(FileInterceptor('file',{
-          storage:diskStorage({
-            destination:"./uploadedFiles", filename: (req, file, cb) => {
-              // Generating a 32 random chars long string
-              const randomName = Array(10).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
-              //Calling the callback passing the random name generated with the original extension name
-              cb(null, `${randomName}${extname(file.originalname)}`)
-            }
-          })
-        })) 
-        async updateUserSheet(
-          @Param('id') id: number,
-          @UploadedFile() file: Multer.File, 
-        ) {
-          // Pass image along with other payload data to service for update
-          return this.sheetsService.updateUserSheet(id, file);
-        }
+  @Post('upload/:id')
+  @UseInterceptors(FileInterceptor('image'))
+  async updateSheet(
+    @Param('id') id: number,
+    @UploadedFile() imageFile: Multer.File,
+  ) {
+    try {
+      return await this.sheetsService.updateSheet(id, imageFile);
+    } catch (error) {
+      console.error('Error uploading sheet:', error);
+      return { error: true, message: 'Error uploading sheet' };
+    }
+  }
 
+  @Post('upload-file/:id')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploadedFiles',
+      filename: (req, file, cb) => {
+        const randomName = Array(10).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+        cb(null, `${randomName}${extname(file.originalname)}`);
+      }
+    })
+  }))
+  async updateUserSheet(
+    @Param('id') id: number,
+    @UploadedFile() uploadedFile: Multer.File,
+  ) {
+    try {
+      return await this.sheetsService.updateUserSheet(id, uploadedFile);
+    } catch (error) {
+      console.error('Error uploading user sheet:', error);
+      return { error: true, message: 'Error uploading user sheet' };
+    }
+  }
 }
--- src/sheets/sheets.service.ts
+++ src/sheets/sheets.service.ts
@@ -3,8 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Sheets } from './sheets.entity';
 import { Repository } from 'typeorm';
 import { Readable } from 'stream';
-import { Users } from 'src/auth/users.entity';
 import csvParser from 'csv-parser';
+import { Users } from 'src/auth/users.entity';
 import { UserSheets } from 'src/userSheets/userSheets.entity';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -12,7 +12,7 @@ import * as csvWriter from 'csv-writer';
 
 @Injectable()
 export class SheetsService {
-  phoneColumns = [
+  phoneColumns = new Set([
     'Phone number',
     'phone_number',
     'Phone',
@@ -22,7 +22,8 @@ export class SheetsService {
     'Mobile number',
     'Cell',
     'Cell Phone',
-  ];
+  ]);
+
   constructor(
     @InjectRepository(Sheets)
     private readonly sheetsRepository: Repository<Sheets>,
@@ -30,85 +31,124 @@ export class SheetsService {
     private readonly userRepository: Repository<Users>,
     @InjectRepository(UserSheets)
     private readonly userSheetsRepository: Repository<UserSheets>,
-  ) {}
+  ) { }
+
+  private async getUser(id: number) {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  private async insertPhoneNumbers(phoneNumbers) {
+    return await this.sheetsRepository.insert(phoneNumbers);
+  }
+
+  private async processCSVFile(bufferStream, user) {
+    const results = [];
+    bufferStream.pipe(csvParser()).on('data', (row) => {
+      for (const key of Object.keys(row)) {
+        if (this.phoneColumns.has(key.toLowerCase())) {
+          results.push({ phone: row[key], user });
+          break;
+        }
+      }
+    });
+    return new Promise((resolve, reject) => {
+      bufferStream
+        .on('end', async () => {
+          try {
+            const existingNumbers = await this.sheetsRepository.find();
+            const uniquePhoneNumbers = results.filter((newEntry) => {
+              return !existingNumbers.some(
+                (existingEntry) => existingEntry.phone === newEntry.phone,
+              );
+            });
+            if (uniquePhoneNumbers.length > 0) {
+              await this.insertPhoneNumbers(uniquePhoneNumbers);
+            }
+            resolve({ error: false, message: 'Sheet uploaded' });
+          } catch (error) {
+            reject({ error: true, message: error?.message });
+          }
+        })
+        .on('error', (error) => {
+          console.error('Error parsing CSV:', error);
+          reject({ error: true, message: error?.message });
+        });
+    });
+  }
+
+  private async processCSVFileForUser(
+    inputFilePath,
+    outputFilePath,
+    adminRecords,
+  ) {
+    if (fs.existsSync(inputFilePath)) {
+      const rows = [];
+      const stream = fs
+        .createReadStream(inputFilePath)
+        .pipe(csvParser())
+        .on('data', (row) => {
+          const phoneNumberColumn = Object.keys(row).find((column) =>
+            Array.from(this.phoneColumns).some((phoneColumn) =>
+              column.toLowerCase().includes(phoneColumn.toLowerCase()),
+            ),
+          );
+          if (phoneNumberColumn) {
+            const phoneNumber = row[phoneNumberColumn];
+            const isHighlighted = adminRecords.some(
+              (adminRow) => adminRow['phone'] === phoneNumber,
+            );
+            if (!isHighlighted) {
+              rows.push(row);
+            }
+          } else {
+            console.log('No phone number column found.');
+          }
+        })
+        .on('end', async () => {
+          if (rows.length > 0) {
+            const csvWriterStream = csvWriter.createObjectCsvWriter({
+              path: outputFilePath,
+              header: Object.keys(rows[0]).map((header) => ({
+                id: header,
+                title: header,
+              })),
+            });
+            await csvWriterStream.writeRecords(rows);
+            console.log('CSV writing completed.');
+          } else {
+            console.log('No matching rows found. Output file not created.');
+          }
+        })
+        .on('error', (error) => {
+          console.error('Error processing CSV:', error);
+        });
+    } else {
+      console.log('File not found.');
+    }
+  }
 
   async updateSheet(
     id: number,
     file,
   ): Promise<{ error: boolean; message: string }> {
     try {
-      const user = await this.userRepository.findOne({ where: { id } });
-      if (!user) {
+      const user = await this.getUser(id);
+      if (!user || user.role !== 'admin') {
         return {
           error: true,
-          message: 'Invalid Id.',
+          message: 'Invalid user or user cannot perform this action.',
         };
       }
-      if (user?.role != 'admin') {
-        return {
-          error: true,
-          message: 'This user cannot perform this action.',
-        };
-      }
-
-      const processingPromise = new Promise(async (resolve, reject) => {
-        const results = []; // Array to store extracted phone numbers
-
-        const bufferStream = new Readable({
-          read() {
-            this.push(file.buffer); // Push the entire buffer to the stream
-            this.push(null); // Signal end of stream
-          },
-        });
-
-        bufferStream
-          .pipe(csvParser()) // Adjust delimiter if needed
-          .on('data', (row) => {
-            // Extract phone number using regular expression or library
-            for (const key in row) {
-              // Check if the current key is 'Phone number'
-              if (
-                this.phoneColumns.some(
-                  (column) => column.toLowerCase() === key.toLowerCase(),
-                )
-              ) {
-                // Access and print the value
-                results.push(row[key]);
-                break;
-              }
-            }
-          })
-          .on('end', async () => {
-            const phoneNumbers = results.map((phoneNumber) => ({
-              phone: phoneNumber,
-              user: user,
-            })); // Prepare data
-            const existingNumbers = await this.sheetsRepository.find();
-            const uniquePhoneNumbers = phoneNumbers.filter((newEntry) => {
-              return !existingNumbers.some(
-                (existingEntry) => existingEntry.phone === newEntry.phone,
-              );
-            });
-            try {
-              if (uniquePhoneNumbers.length > 0) {
-                await this.sheetsRepository.insert(uniquePhoneNumbers);
-              }
-              resolve({ error: false, message: 'Sheet uploaded' });
-            } catch (error) {
-              reject({ error: true, message: error?.message });
-            }
-          })
-          .on('error', (error) => {
-            console.error('Error parsing CSV:', error);
-            reject({ error: true, message: error?.message });
-          });
+      const bufferStream = new Readable({
+        read() {
+          this.push(file.buffer);
+          this.push(null);
+        },
       });
-
-      await processingPromise;
-
+      await this.processCSVFile(bufferStream, user);
       return { error: false, message: 'Sheet Uploaded.' };
     } catch (error) {
-      console.log('error :>> ', error);
+      console.error('Error:', error);
       return { error: true, message: error.message };
     }
   }
@@ -118,12 +158,9 @@ export class SheetsService {
     file,
   ): Promise<{ error: boolean; message: string }> {
     try {
-      const user = await this.userRepository.findOne({ where: { id } });
+      const user = await this.getUser(id);
       if (!user) {
-        return {
-          error: true,
-          message: 'Invalid User Id.',
-        };
+        return { error: true, message: 'Invalid User Id.' };
       }
       const newUserSheet = await this.userSheetsRepository.create({
         fileName: file.filename,
@@ -137,7 +174,6 @@ export class SheetsService {
         return { error: true, message: 'Something went wrong.' };
       }
       const adminRecords = await this.sheetsRepository.find();
-
       const inputFilePath = path.join(
         __dirname,
         '../../uploadedFiles',
@@ -148,69 +184,14 @@ export class SheetsService {
         '../../uploadedFiles',
         `cleaned_${file.filename}`,
       );
-
-      if (fs.existsSync(inputFilePath)) {
-        console.log('Input file found at:', inputFilePath);
-
-        const processingPromise = new Promise((resolve, reject) => {
-          const rows = [];
-          const stream = fs
-            .createReadStream(inputFilePath)
-            .pipe(csvParser())
-            .on('data', (row) => {
-              const phoneNumberColumn = Object.keys(row).find((column) =>
-                this.phoneColumns.some((phoneColumn) =>
-                  column.toLowerCase().includes(phoneColumn.toLowerCase()),
-                ),
-              );
-              if (phoneNumberColumn) {
-                const phoneNumber = row[phoneNumberColumn];
-                const isHighlighted = adminRecords.some(
-                  (adminRow) => adminRow['phone'] === phoneNumber,
-                );
-                if (!isHighlighted) {
-                  rows.push(row);
-                }
-              } else {
-                console.log('No phone number column found.');
-              }
-            })
-            .on('end', () => {
-              if (rows.length > 0) {
-                const csvWriterStream = csvWriter.createObjectCsvWriter({
-                  path: outputFilePath,
-                  header: Object.keys(rows[0]).map((header) => ({
-                    id: header,
-                    title: header,
-                  })),
-                });
-                csvWriterStream
-                  .writeRecords(rows)
-                  .then(() => {
-                    console.log('CSV writing completed.');
-                    resolve({ error: false, message: 'Sheet uploaded' });
-                  })
-                  .catch((error) => {
-                    console.error('Error writing CSV:', error);
-                    reject(error);
-                  });
-              } else {
-                console.log('No matching rows found. Output file not created.');
-                resolve({ error: false, message: 'Sheet uploaded' });
-              }
-            })
-            .on('error', (error) => {
-              console.error('Error processing CSV:', error);
-              reject(error);
-            });
-        });
-
-        await processingPromise;
-      } else {
-        console.log('File not found.');
-      }
+      await this.processCSVFileForUser(
+        inputFilePath,
+        outputFilePath,
+        adminRecords,
+      );
       return { error: false, message: 'Sheet uploaded.' };
     } catch (error) {
+      console.error('Error:', error);
       return { error: true, message: error.message };
     }
   }
