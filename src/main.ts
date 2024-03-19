5875d519e0071e9ed27563bd31c2a3605a150806
Sarab Ali <sarab@contrivers.dev>
2024-03-19 12:56:22 +0500

    added timeout for requests

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const expressApp = app.getHttpAdapter().getInstance()
   app.enableCors({
-    origin: '*'
+    origin: 'https://scrub.callhub.cc'
   });
+  app.use(300000, (req, res, next) => {
+    res.setTimeout(() => {
+      res.status(408).send('Request Timeout')
+    });
+    next()
+  })
+
   await app.listen(8000);
 }
 bootstrap();
