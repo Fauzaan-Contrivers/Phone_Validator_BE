559ce00721fee7ba26d924053e5625ae4efa18ff
Sarab Ali <sarab@contrivers.dev>
2024-03-19 11:34:09 +0500

    enable cors for domain

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://scrub.callhub.cc/'
+  });
   await app.listen(8000);
 }
 bootstrap();
