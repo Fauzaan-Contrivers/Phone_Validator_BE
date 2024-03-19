08f048dae7e66f3fd861ea21ba728ea01d1ae5bb
sarabdev <sarab@contrivers.dev>
2024-03-19 14:55:42 +0500

    enable cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const expressApp = app.getHttpAdapter().getInstance()
-  app.enableCors({
-    origin: 'https://scrub.callhub.cc'
-  });
-  app.use(300000, (req, res, next) => {
-    res.setTimeout(() => {
-      res.status(408).send('Request Timeout')
-    });
-    next()
-  })
+  app.enableCors()
 
   await app.listen(8000);
 }
